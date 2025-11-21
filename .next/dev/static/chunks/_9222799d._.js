f96a502209e81c4b9b727124f7c9e4dbeed79db7
Limberg Montes <nostemlucas@gmail.com>
2025-11-21 09:17:59 -0400

    version alpha

--- .next/dev/static/chunks/_9222799d._.js
+++ .next/dev/static/chunks/_9222799d._.js
@@ -109,7 +109,7 @@ function Header() {
                             }, this),
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("span", {
                                 className: "text-xl font-bold bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent dark:from-primary dark:to-secondary dark:text-transparent",
-                                children: "Tablón Discreto"
+                                children: "Damas de Compañia"
                             }, void 0, false, {
                                 fileName: "[project]/components/header.tsx",
                                 lineNumber: 15,
